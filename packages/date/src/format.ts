c75a80a79dbd9087bcaa2d3aca03edd73fa6fa95
stardustrain <kthanterran@gmail.com>
2021-08-05 17:21:49 +0900

    Add compareDay function

--- packages/date/src/format.ts
+++ packages/date/src/format.ts
@@ -2,11 +2,11 @@ import dateFnsFormat from 'date-fns/format'
 import getMonth from 'date-fns/getMonth'
 import compareAsc from 'date-fns/compareAsc'
 import compareDesc from 'date-fns/compareDesc'
+import getDay from 'date-fns/getDay'
 
 const DEFAULT_FORMAT = 'yyyy-MM-dd HH:mm'
 export const format = (date = new Date(), formatString?: string) => dateFnsFormat(date, formatString ?? DEFAULT_FORMAT)
 
 const compareFn = (isAsc: boolean) => isAsc ? compareAsc : compareDesc
 export const compareMonth = (isAsc = true, ...dates: Date[]) => dates.map(getMonth).sort(compareFn(isAsc))
-
-
+export const compareDay = (isAsc = true, ...dates: Date[]) => dates.map(getDay).sort(compareFn(isAsc))
