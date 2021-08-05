71a4820c5aafb15595f11685e1b3bca677d67af8
stardustrain <kthanterran@gmail.com>
2021-08-05 16:01:22 +0900

    Add compare flag

--- packages/date/src/format.ts
+++ packages/date/src/format.ts
@@ -1,7 +1,9 @@
 import dateFnsFormat from 'date-fns/format'
 import getMonth from 'date-fns/getMonth'
 import compareAsc from 'date-fns/compareAsc'
+import compareDesc from 'date-fns/compareDesc'
 
 const DEFAULT_FORMAT = 'yyyy-MM-dd HH:mm:ss'
 export const format = (date = new Date(), formatString?: string) => dateFnsFormat(date, formatString ?? DEFAULT_FORMAT)
-export const compareMonth = (...dates: Date[]) => dates.map(getMonth).sort(compareAsc)
+const compareFn = (isAsc: boolean) => isAsc ? compareAsc : compareDesc
+export const compareMonth = (isAsc = true, ...dates: Date[]) => dates.map(getMonth).sort(compareFn(isAsc))
