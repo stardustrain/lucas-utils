f566dd3ebadf8c9ba612f6d9729817b79ebf4926
stardustrain <kthanterran@gmail.com>
2021-08-05 13:42:38 +0900

    Fix format string

--- packages/date/src/format.ts
+++ packages/date/src/format.ts
@@ -1,4 +1,4 @@
 import dateFnsFormat from 'date-fns/format'
 
-const DEFAULT_FORMAT = 'yyyy-MM-dd HH:mm'
+const DEFAULT_FORMAT = 'yyyy-MM-dd HH:mm:ss'
 export const format = (date = new Date(), formatString?: string) => dateFnsFormat(date, formatString ?? DEFAULT_FORMAT)
