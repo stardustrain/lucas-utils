5d164ee190f3049a13f85dc31ec6b0cb9b829449
stardustrain <kthanterran@gmail.com>
2021-08-05 17:15:01 +0900

    Fix default format string

--- packages/date/src/format.ts
+++ packages/date/src/format.ts
@@ -3,7 +3,10 @@ import getMonth from 'date-fns/getMonth'
 import compareAsc from 'date-fns/compareAsc'
 import compareDesc from 'date-fns/compareDesc'
 
-const DEFAULT_FORMAT = 'yyyy-MM-dd HH:mm:ss'
+const DEFAULT_FORMAT = 'yyyy-MM-dd HH:mm'
 export const format = (date = new Date(), formatString?: string) => dateFnsFormat(date, formatString ?? DEFAULT_FORMAT)
+
 const compareFn = (isAsc: boolean) => isAsc ? compareAsc : compareDesc
 export const compareMonth = (isAsc = true, ...dates: Date[]) => dates.map(getMonth).sort(compareFn(isAsc))
+
+
